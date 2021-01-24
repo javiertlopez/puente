cf2b1a3dcb13043dba1429089d25479b234767ed
Hiram Torres Lopez <hiram@jtl.mx>
2021-01-24 11:44:34 -0700

    add responseWritter wrapper

--- logger.go
+++ logger.go
@@ -7,21 +7,37 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+type responseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+// ResponseWriter returns a responseWritter wrapper to access the http status
+func newResponseWriter(w http.ResponseWriter) *responseWriter {
+	return &responseWriter{w, http.StatusOK}
+}
+
+// WriteHeader keeps the status code
+func (r *responseWriter) WriteHeader(code int) {
+	r.statusCode = code
+	r.ResponseWriter.WriteHeader(code)
+}
+
 // Logging middleware logs the request
 func (m *Middleware) Logging(next http.Handler) http.Handler {
 	return http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
 			start := time.Now()
 
-			next.ServeHTTP(w, r)
+			wrapped := newResponseWriter(w)
+			next.ServeHTTP(wrapped, r)
 
 			log.WithFields(log.Fields{
-				"status":         r.Response.Status,
-				"method":         r.Method,
-				"path":           r.URL.EscapedPath(),
-				"duration":       time.Since(start),
-				"content-length": r.ContentLength,
-				"x-origin-id":    r.Header.Get("x-origin-id"),
+				"status":      wrapped.statusCode,
+				"method":      r.Method,
+				"path":        r.URL.EscapedPath(),
+				"duration":    time.Since(start),
+				"x-origin-id": r.Header.Get("x-origin-id"),
 			}).Info()
 		},
 	)
